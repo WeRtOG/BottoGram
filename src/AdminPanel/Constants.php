fc13546008e12be3059c5e0aa669e8a4fab3f158
WeRtOG <wertog12@gmail.com>
2021-06-09 05:33:23 +0200

    Fix #4

--- src/AdminPanel/Constants.php
+++ src/AdminPanel/Constants.php
@@ -4,7 +4,7 @@
         BottoGram
     */
 
-    require_once '../Constants.php';
+    require_once __DIR__ . '/../Constants.php';
     
     define('BOTTOGRAM_ADMIN_ROOT', __DIR__);
     define('BOTTOGRAM_ADMIN_ASSETS', BOTTOGRAM_ADMIN_ROOT . '/assets');

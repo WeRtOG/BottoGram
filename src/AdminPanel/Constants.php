fda34881fb8aa39cb0925e95d91c2c2228e0b0e6
WeRtOG <wertog12@gmail.com>
2021-06-09 05:31:58 +0200

    Fix #3

--- src/AdminPanel/Constants.php
+++ src/AdminPanel/Constants.php
@@ -3,6 +3,8 @@
         WeRtOG
         BottoGram
     */
+
+    require_once '../Constants.php';
     
     define('BOTTOGRAM_ADMIN_ROOT', __DIR__);
     define('BOTTOGRAM_ADMIN_ASSETS', BOTTOGRAM_ADMIN_ROOT . '/assets');

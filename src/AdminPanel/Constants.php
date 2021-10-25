8cc90230f9204ae38cab65492206ca2175d08e17
Andrey Vynogradov <40463796+WeRtOG@users.noreply.github.com>
2021-10-25 03:49:18 +0200

    Bug fix

--- src/AdminPanel/Constants.php
+++ src/AdminPanel/Constants.php
@@ -7,7 +7,7 @@
 
 require_once __DIR__ . '/../Constants.php';
 
-define('BOTTOGRAM_ADMIN_ROOT', __DIR__ . '/App-Root');
+define('BOTTOGRAM_ADMIN_ROOT', __DIR__ . '/App-root');
 define('BOTTOGRAM_ADMIN_ASSETS', BOTTOGRAM_ADMIN_ROOT . '/public/assets');
 
 define('BOTTOGRAM_ADMIN_REQUESTLOGSLIMIT', 20);
